ca1008c30d7a4684efc7d87e69225c710a9e6ee6
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-05 14:20:09 +0300

    initial commit

---
+++ src/main/java/com/agan/onlinebuysellplatform/OnlineBuySellPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.agan.onlinebuysellplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBuySellPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBuySellPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agan/onlinebuysellplatform/OnlineBuySellPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agan.onlinebuysellplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBuySellPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
