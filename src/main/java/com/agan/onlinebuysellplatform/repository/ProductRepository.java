580f788b2b373aa8116e9b1b80a774a67e8ea15d
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-08 00:36:05 +0300

    Product Cont-Serv-Repo adapted to Image

--- src/main/java/com/agan/onlinebuysellplatform/repository/ProductRepository.java
+++ src/main/java/com/agan/onlinebuysellplatform/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package com.agan.onlinebuysellplatform.repository;
 
 import com.agan.onlinebuysellplatform.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Transactional(readOnly = true)
     List<Product> findByTitle(String title);
 
 }
--- src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
+++ src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
@@ -1,12 +1,18 @@
 package com.agan.onlinebuysellplatform.service;
 
+import com.agan.onlinebuysellplatform.model.Image;
 import com.agan.onlinebuysellplatform.model.Product;
 import com.agan.onlinebuysellplatform.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 @Slf4j
@@ -15,22 +21,59 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional(readOnly = true)
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
 
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product) {
-        log.info("Saving new {}", product);
+    @Transactional
+    public void saveProduct(Product product, MultipartFile... files) throws IOException {
+        AtomicBoolean isFirstImage = new AtomicBoolean(true);
+
+        Arrays.stream(files)
+                        .filter(file -> file.getSize() != 0)
+                        .map(file -> {
+                            try {
+                                Image image = toImageEntity(file);
+
+                                if (isFirstImage.getAndSet(false)) {
+                                    image.setPreviewImage(true);
+                                }
+
+                                return image;
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .forEach(product::addImageToProduct);
+
+        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor());
+
+        Product productFromDB = productRepository.save(product);
+        productFromDB.setPreviewImageId(productFromDB.getImages().get(0).getId());
 
         productRepository.save(product);
     }
 
+    private Image toImageEntity(MultipartFile file) throws IOException {
+        Image image = new Image();
+        image.setName(file.getName());
+        image.setOriginalFileName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setSize(file.getSize());
+        image.setBytes(file.getBytes());
+
+        return image;
+    }
+
+    @Transactional
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
 
+    @Transactional(readOnly = true)
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
