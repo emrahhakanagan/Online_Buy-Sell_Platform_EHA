a71e4869fb52a2835520bd8bc223040b141c9b6a
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-08 23:41:30 +0300

    UserRepository added for User operations with DB

---
+++ src/main/java/com/agan/onlinebuysellplatform/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.agan.onlinebuysellplatform.repository;
+
+import com.agan.onlinebuysellplatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
