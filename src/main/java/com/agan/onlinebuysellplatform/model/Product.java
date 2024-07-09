489e8fe2e7410f1e73af5f0cf81966ef7e6dcd08
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-10 00:23:33 +0300

    Field Author deleted, now author is username

--- src/main/java/com/agan/onlinebuysellplatform/model/Product.java
+++ src/main/java/com/agan/onlinebuysellplatform/model/Product.java
@@ -27,8 +27,6 @@ public class Product {
     private int price;
     @Column(name = "city")
     private String city;
-    @Column(name = "author")
-    private String author;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
     mappedBy = "product")
     private List<Image> images = new ArrayList<>();
