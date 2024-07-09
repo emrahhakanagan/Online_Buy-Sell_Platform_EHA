d74db867f48b39ad8c7bff43652bec2e53f32959
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-10 00:18:55 +0300

    search method product  by usernam from auth added

--- src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
+++ src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
@@ -2,7 +2,9 @@ package com.agan.onlinebuysellplatform.service;
 
 import com.agan.onlinebuysellplatform.model.Image;
 import com.agan.onlinebuysellplatform.model.Product;
+import com.agan.onlinebuysellplatform.model.User;
 import com.agan.onlinebuysellplatform.repository.ProductRepository;
+import com.agan.onlinebuysellplatform.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -20,6 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public List<Product> listProducts(String title) {
@@ -29,8 +33,9 @@ public class ProductService {
     }
 
     @Transactional
-    public void saveProduct(Product product, MultipartFile... files) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile... files) throws IOException {
         AtomicBoolean isFirstImage = new AtomicBoolean(true);
+        product.setUser(getUserByPrincipal(principal));
 
         Arrays.stream(files)
                         .filter(file -> file.getSize() != 0)
@@ -55,6 +60,12 @@ public class ProductService {
         productFromDB.setPreviewImageId(productFromDB.getImages().get(0).getId());
 
         productRepository.save(product);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if (principal == null) return new User();
+
+        return userRepository.findByEmail(principal.getName());
     }
 
     private Image toImageEntity(MultipartFile file) throws IOException {
