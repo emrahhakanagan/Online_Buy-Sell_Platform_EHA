7633ed7d2f70cd5e2ba465a1306a39a3812dd6d6
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-13 00:00:03 +0300

    uptaded SecurityConfig

--- src/main/java/com/agan/onlinebuysellplatform/configuration/SecurityConfig.java
+++ src/main/java/com/agan/onlinebuysellplatform/configuration/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+
 @RequiredArgsConstructor
 public class SecurityConfig {
 
