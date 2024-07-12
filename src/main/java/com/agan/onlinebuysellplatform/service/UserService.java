ce196f63e627400a560bb7ec9585c46cf842e5e8
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-12 23:56:46 +0300

    uptaded log.info for field username email

--- src/main/java/com/agan/onlinebuysellplatform/service/UserService.java
+++ src/main/java/com/agan/onlinebuysellplatform/service/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -25,7 +27,7 @@ public class UserService {
 
         user.setActive(true);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.getRoles().add(Role.ROLE_USER);
+        user.getRoles().add(Role.ROLE_ADMIN);
 
         log.info("Saving new User with email: {}", email);
         userRepository.save(user);
@@ -33,4 +35,18 @@ public class UserService {
         return true;
     }
 
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user != null) {
+            user.setActive(false);
+            log.info("Ban user with id = {}; email: {}", user.getId(), user.getEmail());
+
+            userRepository.save(user);
+        }
+    }
 }
