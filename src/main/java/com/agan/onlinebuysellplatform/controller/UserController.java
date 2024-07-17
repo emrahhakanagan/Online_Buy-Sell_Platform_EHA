cefd1d15566475b4bbc2c71191e7be920059dcbf
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-17 15:58:33 +0300

    uptaded UserControl for viewpage User-Info

--- src/main/java/com/agan/onlinebuysellplatform/controller/UserController.java
+++ src/main/java/com/agan/onlinebuysellplatform/controller/UserController.java
@@ -3,11 +3,14 @@ package com.agan.onlinebuysellplatform.controller;
 import com.agan.onlinebuysellplatform.model.User;
 import com.agan.onlinebuysellplatform.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,13 +18,24 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
 
         return "login";
     }
 
+    @GetMapping("/profile")
+    public String profile(Principal principal, Model model) {
+        User user = userService.getUserByPrincipal(principal);
+
+        model.addAttribute("user", user);
+
+        return "profile";
+    }
+
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
 
         return "registration";
     }
@@ -38,11 +52,6 @@ public class UserController {
         return "redirect:/login";
     }
 
-    @GetMapping("/hello")
-    public String securityUrl() {
-
-        return "hello";
-    }
 
     @GetMapping("/user/{user}")
     public String userInfo(@PathVariable("user") User user, Model model) {
