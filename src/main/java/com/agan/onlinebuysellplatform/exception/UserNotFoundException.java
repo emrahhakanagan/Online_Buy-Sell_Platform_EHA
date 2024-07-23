b9d128c56c822b23bc79e3fefe258bb89e2678c0
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-23 13:19:48 +0300

    added personal exception for UserNotFoundException

---
+++ src/main/java/com/agan/onlinebuysellplatform/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.agan.onlinebuysellplatform.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
