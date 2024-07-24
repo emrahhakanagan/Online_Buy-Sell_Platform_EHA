5af5cafca1d7cc4bb2c02a6e7abab950d5b77cc7
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-24 23:51:50 +0300

    optimized the files ImageService and ProductService for the successful saving of a new product with image(s)

--- src/main/java/com/agan/onlinebuysellplatform/service/ImageService.java
+++ src/main/java/com/agan/onlinebuysellplatform/service/ImageService.java
@@ -14,6 +14,7 @@ public class ImageService {
 
     @Transactional(readOnly = true)
     public Image getImageById(Long id) {
-        return imageRepository.findById(id).orElse(null);
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Image not found"));
     }
 }
--- src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
+++ src/main/java/com/agan/onlinebuysellplatform/service/ProductService.java
@@ -6,16 +6,14 @@ import com.agan.onlinebuysellplatform.model.User;
 import com.agan.onlinebuysellplatform.repository.ProductRepository;
 import com.agan.onlinebuysellplatform.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 @Slf4j
@@ -29,28 +27,27 @@ public class ProductService {
         return productRepository.findAll();
     }
 
-    public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+
+    public void saveProduct(Principal principal, Product product, MultipartFile... files) {
         product.setUser(getUserByPrincipal(principal));
-        Image image1;
-        Image image2;
-        Image image3;
-        if (file1.getSize() != 0) {
-            image1 = toImageEntity(file1);
-            image1.setPreviewImage(true);
-            product.addImageToProduct(image1);
-        }
-        if (file2.getSize() != 0) {
-            image2 = toImageEntity(file2);
-            product.addImageToProduct(image2);
-        }
-        if (file3.getSize() != 0) {
-            image3 = toImageEntity(file3);
-            product.addImageToProduct(image3);
-        }
+
+        List<Image> images = Arrays.stream(files)
+                .filter(file -> file.getSize() != 0)
+                .map(this::toImageEntity)
+                .peek(image -> {
+                    if (product.getImages().isEmpty()) {
+                        image.setPreviewImage(true);
+                    }
+                    product.addImageToProduct(image);
+                })
+                .toList();
+
         log.info("Saving new Product. Title: {}; Author email: {}", product.getTitle(), product.getUser().getEmail());
+
         Product productFromDb = productRepository.save(product);
         productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
-        productRepository.save(product);
+
+        productRepository.save(productFromDb);
     }
 
     public User getUserByPrincipal(Principal principal) {
@@ -58,7 +55,8 @@ public class ProductService {
         return userRepository.findByEmail(principal.getName());
     }
 
-    private Image toImageEntity(MultipartFile file) throws IOException {
+    @SneakyThrows
+    private Image toImageEntity(MultipartFile file) {
         Image image = new Image();
         image.setName(file.getName());
         image.setOriginalFileName(file.getOriginalFilename());
