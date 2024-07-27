51daafd5c4bb9bf92c2eb9682c18e14f184b751e
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-27 11:45:32 +0300

    Added in ProductRepository a new method for searching products with any symbol keyword for users

--- src/main/java/com/agan/onlinebuysellplatform/repository/ProductRepository.java
+++ src/main/java/com/agan/onlinebuysellplatform/repository/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.agan.onlinebuysellplatform.repository;
 
 import com.agan.onlinebuysellplatform.model.Product;
-import com.agan.onlinebuysellplatform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     List<Product> findByTitle(String title);
+
+    @Query("SELECT p FROM Product p WHERE p.title LIKE %:keyword%")
+    List<Product> searchByTitle(@Param("keyword") String keyword);
+
 }
