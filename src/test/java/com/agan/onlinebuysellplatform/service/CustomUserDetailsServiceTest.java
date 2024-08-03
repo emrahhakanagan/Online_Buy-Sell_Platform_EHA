edc5be3798036164f6471553cff3d56eb69c0902
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-08-03 17:16:11 +0300

    Added tests for CustomUserDetailsService

---
+++ src/test/java/com/agan/onlinebuysellplatform/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,76 @@
+package com.agan.onlinebuysellplatform.service;
+
+import com.agan.onlinebuysellplatform.model.User;
+import com.agan.onlinebuysellplatform.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class CustomUserDetailsServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CustomUserDetailsService customUserDetailsService;
+
+    private String username;
+    private User user;
+
+    @BeforeEach
+    public void setUpMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @BeforeEach
+    public void setUpUser() {
+        username = "test@example.com";
+        user = new User();
+        user.setEmail(username);
+        user.setPassword("password");
+    }
+
+    @Test
+    @DisplayName("Should load user by username when user exists and email is confirmed")
+    public void testLoadUserByUsername_WhenUserExistsAndEmailConfirmed() {
+        user.setConfirmed(true);
+        when(userRepository.findByEmail(username)).thenReturn(user);
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+        assertEquals(username, userDetails.getUsername());
+        assertEquals(user.getPassword(), userDetails.getPassword());
+    }
+
+    @Test
+    @DisplayName("Should throw UsernameNotFoundException when user does not exist")
+    public void testLoadUserByUsername_WhenUserDoesNotExist() {
+        String username = "nonexistent@example.com";
+        when(userRepository.findByEmail(username)).thenReturn(null);
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailsService.loadUserByUsername(username);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw UsernameNotFoundException when email is not confirmed")
+    public void testLoadUserByUsername_WhenEmailNotConfirmed() {
+        user.setConfirmed(false);
+        when(userRepository.findByEmail(username)).thenReturn(user);
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+            customUserDetailsService.loadUserByUsername(username);
+        });
+    }
+}
+
