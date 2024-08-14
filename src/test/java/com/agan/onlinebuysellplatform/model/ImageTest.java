0276a4b5a577baba1bb22157c2f1972c80e51c9c
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-08-14 15:18:09 +0300

    Added serialization and deserialization test for Image class

--- src/test/java/com/agan/onlinebuysellplatform/model/ImageTest.java
+++ src/test/java/com/agan/onlinebuysellplatform/model/ImageTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ImageTest {
@@ -83,5 +85,76 @@ public class ImageTest {
         Product product = new Product();
         image.setProduct(product);
         assertEquals(product, image.getProduct());
+    }
+
+    @Test
+    @DisplayName("Should handle null or negative size")
+    public void testSetSize_WithNullOrNegativeValues() {
+        image.setSize(null);
+        assertNull(image.getSize(), "Size should be null when set to null");
+
+        image.setSize(-1024L);
+        assertEquals(-1024L, image.getSize(), "Size should be negative when set to a negative value");
+    }
+
+    @Test
+    @DisplayName("Should handle empty bytes array")
+    public void testSetBytes_WithEmptyArray() {
+        byte[] emptyBytes = new byte[0];
+        image.setBytes(emptyBytes);
+        assertArrayEquals(emptyBytes, image.getBytes(), "Bytes array should be empty");
+    }
+
+    @Test
+    @DisplayName("Should consider two identical images as equal")
+    public void testEqualsAndHashCode_WithIdenticalImages() {
+        Image image1 = new Image();
+        image1.setId(1L);
+        image1.setName("Test Image");
+        image1.setOriginalFileName("test_image.jpg");
+
+        Image image2 = new Image();
+        image2.setId(1L);
+        image2.setName("Test Image");
+        image2.setOriginalFileName("test_image.jpg");
+
+        assertEquals(image1, image2, "Images with the same properties should be equal");
+        assertEquals(image1.hashCode(), image2.hashCode(), "Hash codes of equal images should be the same");
+    }
+
+    @Test
+    @DisplayName("Should consider two different images as not equal")
+    public void testEqualsAndHashCode_WithDifferentImages() {
+        Image image1 = new Image();
+        image1.setId(1L);
+        image1.setName("Test Image 1");
+
+        Image image2 = new Image();
+        image2.setId(2L);
+        image2.setName("Test Image 2");
+
+        assertNotEquals(image1, image2, "Images with different properties should not be equal");
+        assertNotEquals(image1.hashCode(), image2.hashCode(), "Hash codes of different images should not be the same");
+    }
+
+    @Test
+    @DisplayName("Should serialize and deserialize image")
+    public void testSerialization() throws IOException, ClassNotFoundException {
+        image.setId(1L);
+        image.setName("Serialized Image");
+
+        // Serialize the image object to a byte array
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(image);
+        out.flush();
+        byte[] imageData = byteOut.toByteArray();
+
+        // Deserialize the byte array back to an image object
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(imageData);
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+        Image deserializedImage = (Image) in.readObject();
+
+        assertEquals(image, deserializedImage, "Deserialized image should be equal to the original");
     }
 }
