d817124d09584dd58747b1f840d59678222b31b3
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-08-17 13:04:09 +0300

    Added logic to ImageController to set a default image if product doesnt have any images

--- src/main/java/com/agan/onlinebuysellplatform/controller/ImageController.java
+++ src/main/java/com/agan/onlinebuysellplatform/controller/ImageController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 @RestController
@@ -27,14 +30,23 @@ public class ImageController {
         try {
             Image image = imageService.getImageById(id);
 
+            if (image == null || image.getSize() == 0) {
+                byte[] defaultImageBytes = Files.readAllBytes(Paths.get("src/main/resources/static/images/default-product.png"));
+
+                return ResponseEntity.ok()
+                        .header("filename", "default-product.png")
+                        .contentType(MediaType.IMAGE_PNG)
+                        .contentLength(defaultImageBytes.length)
+                        .body(new InputStreamResource(new ByteArrayInputStream(defaultImageBytes)));
+            }
+
             return ResponseEntity.ok()
                     .header("filename", image.getOriginalFileName())
                     .contentType(MediaType.valueOf(image.getContentType()))
                     .contentLength(image.getSize())
                     .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
-        } catch (RuntimeException e) {
+        } catch (RuntimeException | IOException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Image not found");
         }
     }
-
 }
