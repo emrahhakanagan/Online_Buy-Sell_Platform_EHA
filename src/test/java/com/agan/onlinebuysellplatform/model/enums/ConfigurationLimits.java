2fd3d896b1ba3e35c1a69ec400998a44d582cfcb
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-08-25 22:35:52 +0300

    Added enum ConfigurationLimits for managing project limits

---
+++ src/test/java/com/agan/onlinebuysellplatform/model/enums/ConfigurationLimits.java
@@ -0,0 +1,13 @@
+package com.agan.onlinebuysellplatform.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ConfigurationLimits {
+    IMAGE_NUMBER_OF_PRODUCT(3);
+
+    private final int value;
+
+}
