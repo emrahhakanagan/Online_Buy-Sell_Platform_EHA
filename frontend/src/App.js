4edc1b3ee1443a2e9497217d2e67e696ee679a50
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-10 22:58:13 +0300

    feat: Added React frontend with registration form component and basic styling

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+      <div className="App">
+        <RegistrationForm />  {/* Отображаем форму регистрации */}
+      </div>
+  );
+}
+
+export default App;
